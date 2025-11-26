6f13deee913952cfa1f54ba4b190401c2a68aac6
Isen Naiken <isen.naiken@nexomedis.com>
2025-11-26 20:05:49 +0100

    Update output folder path in configuration to reflect new directory structure

--- preprocessing.py
+++ preprocessing.py
@@ -13,7 +13,7 @@ warnings.filterwarnings('ignore')
 
 # Configuration
 input_folder = "/gold/data_feasibility"
-output_folder = "/silver/ube/dataset"
+output_folder = "/silver/ube/extract"
 patch_size = 3000
 region_size = 15000  # Taille des sous-régions (pour lames ~80000x70000)
 stride_region = 15000  # Stride entre sous-régions (non-overlap si = region_size)

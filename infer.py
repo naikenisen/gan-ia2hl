39a5482093232620aeae9dee83733a7206aad81d
Isen Naiken <isen.naiken@nexomedis.com>
2025-12-01 14:15:07 +0100

    Update checkpoint path to use the correct filename for model loading

--- infer.py
+++ infer.py
@@ -16,7 +16,7 @@ hande_path = 'inference/hes.jpg'
 
 os.makedirs('inference', exist_ok=True)
 generator = Generator(MODEL_SCALE).to(device)
-checkpoint_path = "checkpoints/checkpoint.pth"
+checkpoint_path = "checkpoints/checkpoint_1024.pth.pth"
 checkpoint = torch.load(checkpoint_path, map_location=device)
 generator.load_state_dict(checkpoint['generator'])
 

abf28b247826eecf054d03e5d921b225c52b226c
Isen Naiken <isen.naiken@nexomedis.com>
2025-12-02 11:38:20 +0100

    Update virtual environment paths in train.sh for consistency

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@
 #$ -q gpu
 #$ -N ia2hl_train
 cd $WORKDIR
-source /beegfs/data/work/imvia/in156281/ihc4bc/venv/bin/activate
+source /beegfs/data/work/imvia/in156281/ia2hl/venv/bin/activate
 module load python
-export PYTHONPATH=/work/imvia/in156281/ihc4bc/venv/lib/python3.9/site-packages:$PYTHONPATH
+export PYTHONPATH=/work/imvia/in156281/ia2hl/venv/lib/python3.9/site-packages:$PYTHONPATH
 cd $WORKDIR/ia2hl
 python train.py

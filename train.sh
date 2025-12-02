1331e2d8f01e0dfc21c282f12297f4000cdf7f8e
Isen Naiken <isen.naiken@nexomedis.com>
2025-12-02 10:48:05 +0100

    suppression des sorties pour améliorer la clareté

--- train.sh
+++ train.sh
@@ -1,8 +1,6 @@
 #!/bin/ksh 
 #$ -q gpu
-#$ -o result.out
-#$ -j y
-#$ -N ihc4bc_train
+#$ -N ia2hl_train
 cd $WORKDIR
 source /beegfs/data/work/imvia/in156281/ihc4bc/venv/bin/activate
 module load python

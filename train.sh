eded7b559b10769e23e4a6aeb97bc1f1548d0e54
Isen Naiken <isen.naiken@nexomedis.com>
2025-12-05 19:15:48 +0100

    Add TORCH_HOME environment variable to training script

--- train.sh
+++ train.sh
@@ -5,5 +5,6 @@ cd $WORKDIR
 source /beegfs/data/work/imvia/in156281/ia2hl/venv/bin/activate
 module load python
 export PYTHONPATH=/work/imvia/in156281/ia2hl/venv/lib/python3.9/site-packages:$PYTHONPATH
+export TORCH_HOME=/beegfs/data/work/imvia/in156281/ia2hl/torch_cache
 cd $WORKDIR/ia2hl
 python train.py

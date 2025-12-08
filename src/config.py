678de95fd6337bb4c36ca19e8673121f3d6c02e0
Isen Naiken <isen.naiken@nexomedis.com>
2025-12-08 16:35:59 +0100

    Update BASE_HES_PATH and BASE_IHC_PATH to point to the correct extraction directories

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-BASE_HES_PATH = 'ia2hl_data/train/HES'
-BASE_IHC_PATH = 'ia2hl_data/train/CD30'
+BASE_HES_PATH = '/silver/ube/extraction_v1/HES'
+BASE_IHC_PATH = '/silver/ube/extraction_v1/CD30'
 BUFFER_SIZE = 400
 BATCH_SIZE = 4
 IMG_WIDTH = 1024

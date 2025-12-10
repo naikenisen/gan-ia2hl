de14b680e50292a4fa9c560b34ef4383a15b0bbb
Isen Naiken <isen.naiken@nexomedis.com>
2025-12-10 13:43:41 +0100

    batch to 16

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 BASE_HES_PATH = 'dataset_v2/HES'
 BASE_IHC_PATH = 'dataset_v2/CD30'
-BATCH_SIZE = 8
+BATCH_SIZE = 16
 IMG_WIDTH = 1024
 IMG_HEIGHT = 1024
 EPOCHS = 100

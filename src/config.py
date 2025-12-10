bea0e198e44fd46a81929752a6403d939b4c151e
Isen Naiken <isen.naiken@nexomedis.com>
2025-12-10 11:13:59 +0100

    Update dataset paths in config.py to use dataset_v2 directory

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-BASE_HES_PATH = '/silver/ube/extraction_v1/HES'
-BASE_IHC_PATH = '/silver/ube/extraction_v1/CD30'
+BASE_HES_PATH = 'dataset_v2/HES'
+BASE_IHC_PATH = 'dataset_v2/CD30'
 BUFFER_SIZE = 400
 BATCH_SIZE = 4
 IMG_WIDTH = 1024

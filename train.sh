a75771376c9380bc7572786158c4385b0575d2db
Isen Naiken <isen.naiken@nexomedis.com>
2025-12-10 15:26:58 +0100

    update gitignore to exclude train.sh

--- train.sh
+++ train.sh
@@ -14,7 +14,7 @@ python train.py \
     --img_height 256 \
     --lrg 0.0002 \
     --lrd 0.0002 \
-    --batch_size 20 \
-    --epochs 2 \
+    --batch_size 32 \
+    --epochs 100 \
     --lambda_l1 10 \
     --model_scale 0.75

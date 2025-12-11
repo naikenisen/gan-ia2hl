6b6220928bd55adc7392da86e2137ac434888f68
Isen Naiken <isen.naiken@nexomedis.com>
2025-12-11 16:11:35 +0100

    mise à jour des chemins de modèle et des paramètres dans le script d'inférence

--- infer.sh
+++ infer.sh
@@ -1,8 +1,8 @@
 python infer.py \
-    --checkpoint_path best_models/best_model.pth \
+    --checkpoint_path best_models/batch-32-scale-1.0-lambda-10.0-width-256-lrg-0.0001-lrd-0.0001.pth \
     --img_width 256 \
     --img_height 256 \
-    --batch_size 8 \
-    --model_scale 0.75 \
+    --batch_size 32 \
+    --model_scale 1 \
     --base_hes_path dataset_v2/HES \
     --base_ihc_path dataset_v2/CD30

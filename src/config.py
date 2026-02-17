f427cde5e6e9173d5bc6501ff53c07fbee761257
Isen Naiken <isen.naiken@nexomedis.com>
2026-02-17 23:36:40 +0100

    hp

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-IMG_WIDTH = 512
-IMG_HEIGHT = 512
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
 LRG = 1e-4
 LRD = 1e-4
 BASE_HES_PATH = 'dataset_tiled_512/HES'

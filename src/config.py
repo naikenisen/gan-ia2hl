a899bfa8281c8ef45e89946569a0ef773b05837d
Isen Naiken <isen.naiken@nexomedis.com>
2026-02-18 11:07:08 +0100

    update hp

--- src/config.py
+++ src/config.py
@@ -2,14 +2,14 @@ import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-IMG_WIDTH = 512
-IMG_HEIGHT = 512
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
 LRG = 1e-4
 LRD = 1e-4
 BASE_HES_PATH = 'dataset_tiled_512/HES'
 BASE_IHC_PATH = 'dataset_tiled_512/CD30'
 CHECKPOINT_DIR = 'best_models'
 BATCH_SIZE = 32
-EPOCHS = 100
+EPOCHS = 50
 LAMBDA = 10
 MODEL_SCALE = 1

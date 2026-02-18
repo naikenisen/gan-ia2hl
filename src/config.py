d0cb3efe4bf210bb83b7f11d4f93a417fd8ae61f
Isen Naiken <isen.naiken@nexomedis.com>
2026-02-18 20:06:22 +0100

    maj hyperparam

--- src/config.py
+++ src/config.py
@@ -2,14 +2,14 @@ import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
+IMG_WIDTH = 512
+IMG_HEIGHT = 512
 LRG = 1e-4
 LRD = 1e-4
 BASE_HES_PATH = 'dataset/HES'
 BASE_IHC_PATH = 'dataset/CD30'
 CHECKPOINT_DIR = 'best_models'
 BATCH_SIZE = 32
-EPOCHS = 50
+EPOCHS = 70
 LAMBDA = 10
 MODEL_SCALE = 1

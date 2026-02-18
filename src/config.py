27257ff3d65422218f5116b090b13bdd8f51c366
Isen Naiken <isen.naiken@nexomedis.com>
2026-02-18 09:57:52 +0100

    Update image dimensions and training epochs in configuration

--- src/config.py
+++ src/config.py
@@ -2,14 +2,14 @@ import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
+IMG_WIDTH = 512
+IMG_HEIGHT = 512
 LRG = 1e-4
 LRD = 1e-4
 BASE_HES_PATH = 'dataset_tiled_512/HES'
 BASE_IHC_PATH = 'dataset_tiled_512/CD30'
 CHECKPOINT_DIR = 'best_models'
 BATCH_SIZE = 32
-EPOCHS = 50
+EPOCHS = 100
 LAMBDA = 10
 MODEL_SCALE = 1
